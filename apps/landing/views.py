6e266ad8b4fd271d04c4814ddc232d1e76d45a87
mukhagaliaryn <mukhagaliaryn@gmail.com>
2026-02-19 19:34:19 +0500

    Done

--- apps/landing/views.py
+++ apps/landing/views.py
@@ -1,5 +1,6 @@
 from django.shortcuts import render
 
+
 # Landing page
 # ----------------------------------------------------------------------------------------------------------------------
 def landing_view(request):
