6ef56fcc050b5cfecabab99266f90da2c4f83ba1
mukhagaliaryn <mukhagaliaryn@gmail.com>
2026-02-27 09:37:22 +0500

    Done

--- core/admin/books.py
+++ core/admin/books.py
@@ -5,7 +5,7 @@ from core.models.books import Book, UserBook
 
 @admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
-    list_display = ("id", "title", "is_active", "order", "created_at")
+    list_display = ("title", "is_active", "order", "created_at")
     list_filter = ("is_active",)
     search_fields = ("title",)
     ordering = ("order", "-id")
@@ -14,6 +14,6 @@ class BookAdmin(admin.ModelAdmin):
 
 @admin.register(UserBook)
 class UserBookAdmin(admin.ModelAdmin):
-    list_display = ("id", "user", "book", "is_saved", "progress", "last_page", "updated_at")
+    list_display = ("user", "book", "is_saved", "progress", "last_page", "updated_at")
     list_filter = ("is_saved",)
     search_fields = ("user__username", "user__email", "book__title")
